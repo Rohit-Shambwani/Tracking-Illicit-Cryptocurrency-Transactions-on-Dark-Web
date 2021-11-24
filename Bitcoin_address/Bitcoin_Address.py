15ccde9e768e703e8740c5b3faf1ba854b954f5b
Rohit-Shambwani <70758712+Rohit-Shambwani@users.noreply.github.com>
2021-11-24 19:32:35 +0530

    Add files via upload

---
+++ Bitcoin_address/Bitcoin_Address.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+
+
+
+# In[16]:
+
+
+
+
+
+# In[1]:
+
+
+from hashlib import sha256
+ 
+digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+ 
+def decode_base58(bc, length):
+    n = 0
+    for char in bc:
+        n = n * 58 + digits58.index(char)
+    return n.to_bytes(length, 'big')
+def check_bc(bc):
+    try:
+        bcbytes = decode_base58(bc, 25)
+        return bcbytes[-4:] == sha256(sha256(bcbytes[:-4]).digest()).digest()[:4]
+    except Exception:
+        return False
+ 
+print(check_bc('3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy'))
+print(check_bc("17NdbrSGoUotzeGCcMMCqnFkEvLymoou9j"))
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Dark_Web_Crawler
+Dark web Web crawler for Cryptocurrency tracking
+It will scrape the dark web pages for cryptocurrency address and trace the flow of crypto to and from those accounts.
